824c0cd989438f33c24546ad31a4e007125501c5
donghaozhangpeter <dzha9516@uni.sydney.edu.au>
2018-08-16 15:57:14 +1000

    Work on the dataset

--- txttoimg.py
+++ txttoimg.py
@@ -1,10 +1,16 @@
-import numpy as np
-text_file = open("./Digital Pathology_segmentation_training_set/image01_mask.txt", "r")
+from os import path
+import os
+# Load the txt file storing the mask information
+absFilePath = os.path.abspath(__file__)
+folder_name = os.path.dirname(absFilePath)
+filepath = path.join(folder_name, "..", "fast_segmentation/digital_pathology_2018/Digital Pathology_segmentation_training_set/image01_mask.txt")
+text_file = open(filepath, "r")
+
+# Divide text_file into multiple lines so that I can easily index them
 lines = text_file.readlines()
-print(lines[0])
-im_dim = lines[0]
-# x, y = np.loadtxt(im_dim, delimiter=',', usecols=(0, 2), unpack=True)
-test_split = im_dim.split()
-print(test_split)
-print(im_dim[0])
+
+# String format of the image dimension
+txt_im_dim = lines[0]
+txt_im_dim_split = txt_im_dim.split()
+print('the image dimension of the current file is :', txt_im_dim_split[0])
 print('the length of lines', len(lines))
