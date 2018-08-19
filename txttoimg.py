324cc056fd3484f5442b8a5e3176d8e880bf26f1
donghaozhangpeter <dzha9516@uni.sydney.edu.au>
2018-08-19 23:15:02 +1000

    whole  folder

--- txttoimg.py
+++ txttoimg.py
@@ -13,7 +13,7 @@ prefix_txt = "fast_segmentation/digital_pathology_2018/Digital Pathology_segment
 suffix_txt = "_mask.txt"
 im_suffix_txt = ".png"
 
-for file_num in range(0, 16):
+for file_num in range(1, 16):
 	if file_num < 10:
 		file_num_txt = str(0) + str(file_num)
 	else:
@@ -23,43 +23,37 @@ for file_num in range(0, 16):
 	txt_filepath = path.join(folder_name, "..", txt_comb)
 	im_filepath = path.join(folder_name, "..", im_comb)
 
-text_file = open(txt_filepath, "r")
-# Divide text_file into multiple lines so that I can easily index them
-lines = text_file.readlines()
+	text_file = open(txt_filepath, "r")
+	# Divide text_file into multiple lines so that I can easily index them
+	lines = text_file.readlines()
 
-# String format of the image dimension
-txt_im_dim = lines[0]
-# print(lines[1])
-txt_im_dim_split = txt_im_dim.split()
-width_num = int(txt_im_dim_split[0])
-height_num = int(txt_im_dim_split[1])
-print('the image dimension of the current file is :', txt_im_dim_split[0])
-print('the length of lines', len(lines))
-print('width_num and height_num are : ', width_num, height_num)
+	# String format of the image dimension
+	txt_im_dim = lines[0]
+	txt_im_dim_split = txt_im_dim.split()
+	width_num = int(txt_im_dim_split[0])
+	height_num = int(txt_im_dim_split[1])
+	print('the image dimension of the current file is :', txt_im_dim_split[0])
+	print('the length of lines', len(lines))
+	print('width_num and height_num are : ', width_num, height_num)
 
-label_im = np.zeros((width_num, height_num))
-im = Image.open(im_filepath)
-im_np = np.asarray(im)
-# print(im)
-for x in range(0, width_num):
-	for y in range(0, height_num):
-		loc_num = x + y * width_num + 1
-		label_im[x, y] = int(lines[loc_num])
+	label_im = np.zeros((width_num, height_num))
+	im = Image.open(im_filepath)
+	im_np = np.asarray(im)
+	for x in range(0, width_num):
+		for y in range(0, height_num):
+			loc_num = x + y * width_num + 1
+			label_im[x, y] = int(lines[loc_num])
 
-print('the shape of label_im is ', label_im.shape)
-label_im_transpose = np.transpose(label_im, (1, 0))
-print('the shape of label_im_transpose is ', label_im_transpose.shape)
-print('the final loc_num is ', loc_num)
-
-# label_path_prefix = '../fast_segmentation/digital_pathology_2018/Digital Pathology_segmentation_training_set/'
-# label_path_suffix = 'trainannot'
-label_save_path = '/home/donghao/Desktop/donghao/fast_segmentation/digital_pathology_2018/Digital Pathology_segmentation_training_set/trainannot/'
-im_save_path = '/home/donghao/Desktop/donghao/fast_segmentation/digital_pathology_2018/Digital Pathology_segmentation_training_set/train/'
-print(im_comb)
-label_save_path = label_save_path + file_num_txt + '.png'
-im_save_path = im_save_path + file_num_txt + '.png'
-cv2.imwrite(label_save_path, label_im)
-cv2.imwrite(im_save_path, im_np)
+	print('the shape of label_im is ', label_im.shape)
+	label_im_transpose = np.transpose(label_im, (1, 0))
+	print('the shape of label_im_transpose is ', label_im_transpose.shape)
+	print('the final loc_num is ', loc_num)
+	label_save_path = '/home/donghao/Desktop/donghao/fast_segmentation/digital_pathology_2018/Digital Pathology_segmentation_training_set/trainannot/'
+	im_save_path = '/home/donghao/Desktop/donghao/fast_segmentation/digital_pathology_2018/Digital Pathology_segmentation_training_set/train/'
+	label_save_path = label_save_path + file_num_txt + '.png'
+	im_save_path = im_save_path + file_num_txt + '.png'
+	cv2.imwrite(label_save_path, label_im)
+	cv2.imwrite(im_save_path, im_np)
 
 # pil_im.save("./testxx.png")
 # fig1 = plt.figure()
