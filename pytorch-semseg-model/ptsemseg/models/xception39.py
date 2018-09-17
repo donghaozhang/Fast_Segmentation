df88672b188c68a5e8e070acffe8e24898df45f2
donghaozhangpeter <dzha9516@uni.sydney.edu.au>
2018-09-17 14:18:08 +1000

    work on the 3D version

--- pytorch-semseg-model/ptsemseg/models/xception39.py
+++ pytorch-semseg-model/ptsemseg/models/xception39.py
@@ -51,6 +51,18 @@ class SeparableConv2d(nn.Module):
 		x = self.pointwise(x)
 		return x
 
+class SeparableConv3d(nn.Module):
+	def __init__(self, in_channels, out_channels, kernel_size=1, stride=1, padding=0, dilation=1, bias=False):
+		super(SeparableConv3d, self).__init__()
+
+		self.conv1 = nn.Conv3d(in_channels, in_channels, kernel_size, stride, padding, dilation, groups=in_channels, bias=bias)
+		self.pointwise = nn.Conv3d(in_channels, out_channels, 1, 1, 0, 1, 1, bias=bias)
+
+	def forward(self, x):
+		x = self.conv1(x)
+		x = self.pointwise(x)
+		return x
+
 class Block(nn.Module):
 	def __init__(self, in_filters, out_filters, reps, strides=1, start_with_relu=True, grow_first=True):
 		super(Block, self).__init__()
@@ -359,7 +371,56 @@ class Bisenet(nn.Module):
 		# y = self.fc_xception39(y)
 		return y_cat
 
-class
+class Block3D(nn.Module):
+	def __init__(self, in_filters, out_filters, reps, strides=1, start_with_relu=True, grow_first=True):
+		super(Block3D, self).__init__()
+
+		if out_filters != in_filters or strides != 1:
+			self.skip = nn.Conv3d(in_filters, out_filters, 1, stride=strides, bias=False)
+			self.skipbn = nn.BatchNorm3d(out_filters)
+		else:
+			self.skip = None
+
+		self.relu = nn.ReLU(inplace=True)
+		rep = []
+
+		filters = in_filters
+		if grow_first:
+			rep.append(self.relu)
+			rep.append(SeparableConv3d(in_filters, out_filters, 3, stride=1, padding=1, bias=False))
+			rep.append(nn.BatchNorm3d(out_filters))
+			filters = out_filters
+
+		for i in range(reps - 1):
+			rep.append(self.relu)
+			rep.append(SeparableConv3d(filters, filters, 3, stride=1, padding=1, bias=False))
+			rep.append(nn.BatchNorm3d(filters))
+
+		if not grow_first:
+			rep.append(self.relu)
+			rep.append(SeparableConv3d(in_filters, out_filters, 3, stride=1, padding=1, bias=False))
+			rep.append(nn.BatchNorm3d(out_filters))
+
+		if not start_with_relu:
+			rep = rep[1:]
+		else:
+			rep[0] = nn.ReLU(inplace=False)
+
+		if strides != 1:
+			rep.append(nn.MaxPool3d(3, strides, 1))
+		self.rep = nn.Sequential(*rep)
+
+	def forward(self, inp):
+		x = self.rep(inp)
+
+		if self.skip is not None:
+			skip = self.skip(inp)
+			skip = self.skipbn(skip)
+		else:
+			skip = inp
+
+		x += skip
+		return x
 
 class bisenet3D(nn.Module):
 		"""
