65f32f217f12668155ab4767cb4e3ed34aca0731
donghaozhangpeter <dzha9516@uni.sydney.edu.au>
2018-09-23 21:11:40 +1000

    I am working on the bisenet3D

--- pytorch-semseg-model/debug2.py
+++ pytorch-semseg-model/debug2.py
@@ -1,17 +1,18 @@
 from ptsemseg.models.xception39 import xception39
 from ptsemseg.models.xception39 import bisenet
+from ptsemseg.models.xception39 import bisenet3D
 import numpy as np
 import torch
 from torch.autograd import Variable
 
 
-bisenet_model = bisenet(num_classes=1000, pretrained=False)
-bisenet_model.cuda()
+# bisenet_model = bisenet(num_classes=1000, pretrained=False)
+# bisenet_model.cuda()
 # xception_model = xception39(num_classes=1000, pretrained=False)
 # xception_model.cuda()
 
 # 2D version of numpy_fake_image should be your 3D input block
-fake_im_num = 1
+# fake_im_num = 1
 # numpy_fake_image = np.random.rand(fake_im_num, 3, 224, 224)
 # tensor_fake_image = torch.FloatTensor(numpy_fake_image)
 # torch_fake_image = Variable(tensor_fake_image).cuda()
@@ -25,7 +26,9 @@ from scipy.stats import norm
 # print(300*b)
 
 # Try to create 3D fake data
+fake_im_num = 1
+bisenet_model_3D = bisenet3D(num_classes=1000, pretrained=False)
 numpy_fake_image_3d = np.random.rand(fake_im_num, 3, 224, 224, 224)
 tensor_fake_image_3d = torch.FloatTensor(numpy_fake_image_3d)
 torch_fake_image_3d = Variable(tensor_fake_image_3d).cuda()
-output_3d = bisenet_model(torch_fake_image_3d)
+output_3d = bisenet_model_3D(torch_fake_image_3d)
