ade1032e5815482eee4b880d5d092f5eebe391f1
donghaozhangpeter <dzha9516@uni.sydney.edu.au>
2018-10-08 20:24:37 +1100

    I am trying to implement my own loass function

--- history/debug7.py
+++ history/debug7.py
@@ -2,6 +2,8 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 import torch.nn as nn
+from torch.autograd import Function, Variable
+from torch import optim
 
 
 class double_conv(nn.Module):
@@ -108,19 +110,74 @@ class UNet(nn.Module):
 		x = self.outc(x)
 		return x
 
+
+class DiceCoeff(Function):
+	"""Dice coeff for individual examples"""
+
+	def forward(self, input, target):
+		self.save_for_backward(input, target)
+		eps = 0.0001
+		self.inter = torch.dot(input.view(-1), target.view(-1))
+		self.union = torch.sum(input) + torch.sum(target) + eps
+
+		t = (2 * self.inter.float() + eps) / self.union.float()
+		return t
+
+	# This function has only a single output, so it gets only one gradient
+	def backward(self, grad_output):
+		print('has this being called')
+
+		input, target = self.saved_variables
+		grad_input = grad_target = None
+
+		if self.needs_input_grad[0]:
+			grad_input = grad_output * 2 * (target * self.union + self.inter) \
+						 / self.union * self.union
+		if self.needs_input_grad[1]:
+			grad_target = None
+
+		return grad_input, grad_target
+
+
+def dice_coeff(input, target):
+	"""Dice coeff for batches"""
+	if input.is_cuda:
+		s = torch.FloatTensor(1).cuda().zero_()
+	else:
+		s = torch.FloatTensor(1).zero_()
+
+	for i, c in enumerate(zip(input, target)):
+		s = s + DiceCoeff().forward(c[0], c[1])
+
+	return s / (i + 1)
+
+
 fake_im_num = 1
 imgs = np.random.rand(fake_im_num, 4, 256, 256)
 true_masks = np.random.rand(fake_im_num, 256, 256)
 imgs = torch.from_numpy(imgs).float()
-true_masks = torch.from_numpy(true_masks)
+true_masks = torch.from_numpy(true_masks).float()
+true_masks = Variable(true_masks)
 
 if torch.cuda.is_available():
 	imgs = imgs.cuda()
 	true_masks = true_masks.cuda()
 net = UNet(n_channels=4, n_classes=3)
 net = net.cuda()
+optimizer = optim.SGD(net.parameters(),
+					  lr=1e-5,
+					  momentum=0.9,
+					  weight_decay=0.0005)
 masks_pred = net(imgs)
 masks_probs = F.sigmoid(masks_pred)
-print('The size of mask is ', masks_probs.size())
+print('The size of mask_probs_flat is ', masks_probs.size())
+print('The size of true_masks_flat is ', true_masks.size())
 masks_probs_flat = masks_probs.view(-1)
 true_masks_flat = true_masks.view(-1)
+criterion = DiceCoeff()
+print(true_masks_flat.requires_grad)
+loss = criterion(true_masks_flat, true_masks_flat)
+loss = Variable(loss.data)
+optimizer.zero_grad()
+
+loss.backward()
